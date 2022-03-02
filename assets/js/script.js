532a0bee9229ed8c8bba1ef58267859def1f3e67
Kaia Scheirman <kfscheirman18@gmail.com>
2022-03-01 16:16:48 -0800

    check current time and update schedule colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,4 +8,33 @@ $(document).ready(function() {
         // save key value pair to localstorage
         localStorage.setItem(timeStamp, textInput);
     });
+
+    // check time and update colors accordingly
+    var updateTime = function() {
+        // save the current hour in a variable
+        var currentHour = moment().hours();
+
+        $('.time-block').each(function() {
+            // save the schedule hour in a variable
+            var scheduleHour = parseInt($(this).attr("id"));
+            console.log(scheduleHour);
+
+            // if statement to check if the current hour has passed
+            if (scheduleHour < currentHour) {
+                $(this).addClass("past");
+            } else if (scheduleHour === currentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+            } else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            };
+        });
+    };
+
+    updateTime();
+
+    // display the day
+    $('#currentDay').text(moment().format('dddd, MMMM Do'));
 });
