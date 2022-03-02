4dcb1d7f5f77bafd1f95178811496235726f7ae2
Kaia Scheirman <kfscheirman18@gmail.com>
2022-03-01 17:20:11 -0800

    add task persistence

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,13 @@ $(document).ready(function() {
         var timeStamp = $(this).parent().attr('id');
         // save key value pair to localstorage
         localStorage.setItem(timeStamp, textInput);
+
+        // show user that their input was saved
+        $(this).html("Saved!")
+        setTimeout(function() {
+            // switch back to 'save' text after 3 seconds
+            $(".saveBtn").html("Save");
+        }, 1500);
     });
 
     // check time and update colors accordingly
@@ -33,8 +40,22 @@ $(document).ready(function() {
         });
     };
 
+    // check localstorage for saved data and load it onto the page
+    $('#9 .description').val(localStorage.getItem("9"));
+    $('#10 .description').val(localStorage.getItem("10"));
+    $('#11 .description').val(localStorage.getItem("11"));
+    $('#12 .description').val(localStorage.getItem("12"));
+    $('#13 .description').val(localStorage.getItem("13"));
+    $('#14 .description').val(localStorage.getItem("14"));
+    $('#15 .description').val(localStorage.getItem("15"));
+    $('#16 .description').val(localStorage.getItem("16"));
+    $('#17 .description').val(localStorage.getItem("17"));
+  
+
     // call the updateTime function upon loading the page
     updateTime();
+    // check current time every 10 seconds
+    var checkTime = setInterval(updateTime, 10000);
 
     // display the day
     $('#currentDay').text(moment().format('dddd, MMMM Do, YYYY'));
